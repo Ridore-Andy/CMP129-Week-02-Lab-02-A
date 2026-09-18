45054e8aa758ea5910a47929b9c9a4a003eeb7d5
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-18 14:16:42 -0400

    Complete Week 2 lab 2

--- Person.java
+++ Person.java
@@ -1,3 +1,37 @@
-public class Person {
+public class Person 
+{
+    String name;
+
+    int age;
+
+    String email;
     
+    public String getName(String name)
+    {
+        return name;
+    }
+    
+    private String setName(String name)
+    {
+        this.name = "bob";
+        return name;
+    }
+
+     public int getAge(int age) 
+    {
+        return age;
+    }
+
+
+     public String getEmail(String email)
+    {
+        return email;
+    }
+
+    public static void displayInfo(String name, int age, String email)
+    {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(email);
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,5 @@
-public class PersonTest {
+public class PersonTest 
+{
+    
     
 }
